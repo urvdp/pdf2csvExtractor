c947c32867a63d8c93d21ffb1fd860678d5471a0
jan <urvdp@student.kit.edu>
2025-02-24 13:25:45 +0100

    support credit card statements pdf as well

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 
 
 class Config:
-    dev = False
-    pdf_path = '/home/jan/Downloads/konto_jan.pdf'
-    csv_path = 'konto_jan.csv'
+    dev = True
+    #pdf_path = '/home/jan/Downloads/konto_jan.pdf'
+    pdf_path = '/home/jan/Downloads/credit_card.pdf'
+    #csv_path = 'konto_jan.csv'
+    csv_path = 'credit_card.csv'
--- pdf2csv.py
+++ pdf2csv.py
@@ -7,9 +7,10 @@ import pandas as pd
 
 from config import Config
 
+# Regex to capture something like 'dd.mm.yyyy' and then the rest
+date_pattern = r'(\d{2}\.\d{2}\.\d{4})\s*(.*)'
+
 def format_bank_pdf(df: pd.DataFrame) -> pd.DataFrame:
-    # Regex to capture something like 'dd.mm.yyyy' and then the rest
-    date_pattern = r'(\d{2}\.\d{2}\.\d{4})\s*(.*)'
 
     df.rename(columns={
         'Datum Erläuterung': 'date_explanation',
@@ -40,9 +41,45 @@ def format_bank_pdf(df: pd.DataFrame) -> pd.DataFrame:
 
     return df
 
+def format_credit_card_pdf(df: pd.DataFrame) -> pd.DataFrame:
 
+    print(f"Credit card pdf cols: {df.columns}")
+    date_col = df.columns[0]
+    date_col_booking = df.columns[1]
+    explanation_col = df.columns[2]
+    cost_col = df.columns[3]
+    df.rename(columns={
+        date_col: 'date',
+        date_col_booking: 'date_booking',
+        explanation_col: 'explanation',
+        cost_col: 'cost'
+    }, inplace=True)
+    df.drop(columns=['date_booking'], inplace=True)
+    # insert none in empty colums
+    df[['date', 'explanation', 'cost']] = df[['date', 'explanation', 'cost']].replace(r'^\s*$', np.nan, regex=True)
+    # convert date to datetime
+    date_format = "%d.%m.%y"
+    df['date'] = pd.to_datetime(df['date'], format=date_format, errors='coerce')
+    # drop rows with all NaN values
+    df = df.dropna(subset=['cost'], how='all')
+    df = df.dropna(subset=['date'], how='all')
+    # convert to desired day format
+    df['date'] = df['date'].dt.strftime('%d.%m.%Y')
+
+    # get only costs and remove positive values
+    df_filtered = df[df['cost'].str.strip().str.endswith('-')]
+    df_filtered.loc[:, 'cost'] = df_filtered['cost'].str.replace('-', '', regex=False)
+    df_filtered.loc[:, 'cost'] = df_filtered['cost'].str.strip()
+
+    # rearrange columns
+    df_filtered = df_filtered[['date', 'cost', 'explanation']]
+    # insert category column
+    df_filtered.insert(2, "category", "")
+
+    return df_filtered
 
 def main():
+    is_credit_card = False
     if not Config.dev and len(sys.argv) < 3:
         print("Usage: python pdf2csv.py input.pdf output.csv")
         sys.exit(1)
@@ -53,6 +90,11 @@ def main():
         else:
             pdf_file = Config.pdf_path
             csv_file_path = Config.csv_path
+
+    if '-c' in sys.argv:
+        # using credit card config
+        print("### Using credit card config ###")
+        is_credit_card = True
 
     csv_path = os.path.dirname(csv_file_path)
     csv_file = os.path.basename(csv_file_path)
@@ -69,16 +111,23 @@ def main():
         with open(csv_file, 'w') as f:
             pass
 
-
-    tables = tabula.read_pdf(pdf_file, pages='all', stream=True)
+    if is_credit_card:
+        tables = tabula.read_pdf(pdf_file, pages='all',
+                                 area=[350, 30, 650, 580],
+                                 columns=[80, 120, 510, 580]) # custom values for credit card pdf
+    else:
+        tables = tabula.read_pdf(pdf_file, pages='all', stream=True)
     print(f"Tables found: {len(tables)}")
 
     df = pd.DataFrame()
     for table in tables:
-        df = pd.concat([df, format_bank_pdf(table)], ignore_index=True)
+        if is_credit_card:
+            df = pd.concat([df, format_credit_card_pdf(table)], ignore_index=True)
+        else:
+            df = pd.concat([df, format_bank_pdf(table)], ignore_index=True)
 
     df.to_csv(csv_file, index=False, decimal=',')
-    print(f"CSV file written to {csv_path}{csv_file}")
+    print(f"git CSV file written to {csv_path}{csv_file}")
 
 
 if __name__ == "__main__":
